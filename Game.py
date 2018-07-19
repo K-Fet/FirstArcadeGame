d9bbd12ba4761df2551c869c2e04dd1b829645da
bdbipbip <baptiste.dubarry21@gmail.com>
2018-07-20 00:07:04 +0200

    add borders

--- Game.py
+++ Game.py
@@ -8,21 +8,18 @@ class Game(arcade.Window):
     super().__init__(screen_width, screen_height, fullscreen=True)
     arcade.set_background_color(arcade.color.AMAZON)
     self.player = None
-
-    width, heigth = self.get_size()
-    self.set_viewport(0, width, 0, heigth)
-
+    self.screen_width,self.screen_height = self.get_size()
   
   
   def setup(self):
-    self.player = player(0,0)
+    self.player = player(self.screen_width/2,self.screen_height/2)
 
   def on_draw(self): 
     arcade.start_render()
     self.player.draw()
 
   def update(self, delta_time):
-    self.player.update()
+    self.player.update(self.screen_width, self.screen_height)
 
   def on_key_press(self, key, modifiers):
     if key == arcade.key.UP:
@@ -37,9 +34,8 @@ class Game(arcade.Window):
   def on_key_release(self, key, modifiers):
     if key == arcade.key.UP or key == arcade.key.DOWN:
       self.player.change_y = 0
-    elif key == arcade.key.LEFT or key == arcade.key.RIGHT:
+    if key == arcade.key.LEFT or key == arcade.key.RIGHT:
       self.player.change_x = 0
-
     
 def main():
 
