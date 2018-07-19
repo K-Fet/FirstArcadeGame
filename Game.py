75e7971d88c333c3a5df22496c203d8a23623868
Jean-Pierre MACHADO <mesjtarbes.direction@aplb.lan>
2018-07-20 00:07:23 +0200

    Beer and secu list

--- Game.py
+++ Game.py
@@ -7,14 +7,34 @@ class Game(arcade.Window):
     super().__init__(screen_width, screen_height)
     arcade.set_background_color(arcade.color.AMAZON)
   
+    self.beer_sprite=None
+    self.beer_list=None
+
+    self.securitas_sprite=None
+    self.securitas_list=None
+
+    self.all_sprite_list=None
+
   
   def setup(self):
+    self.all_sprite_list=arcade.SpriteList()
 
+    self.beer_list=arcade.SpriteList()
+    self.beer=beer(50,50)
+    self.beer_list.append(self.beer)
+
+    self.securitas_list=arcade.SpriteList()
+    self.securitas=securitas(100,100)
+    self.securitas_list.append(self.securitas)
+    self.all_sprite_list.append(self.securitas)
     pass
 
   def update(self, delta_time):
-
+    self.all_sprite_list.update()
+    self.beer_list.update()
     pass
+  
+  
 
 def main():
 
