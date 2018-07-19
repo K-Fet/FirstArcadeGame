21676ed44063ebe8ded4f255e16b895ca06515f6
bdbipbip <baptiste.dubarry21@gmail.com>
2018-07-20 00:07:04 +0200

    add borders

--- player.py
+++ player.py
@@ -6,5 +6,17 @@ class player(arcade.Sprite):
 		super().__init__("img/player.png",SPRITE_SCALING_PLAYER)
 		self.center_x=center_x
 		self.center_y=center_y
+
+	def update(self, screen_width, screen_heigth):
+		## reset the change_x before updating if player is on screen limit
+		if (self.center_x < BORDERS_OFFSET):
+			if self.change_x < 0 : self.change_x = 0
+		if (self.center_x > screen_width - BORDERS_OFFSET):
+			if self.change_x > 0 : self.change_x = 0
+		if (self.center_y < BORDERS_OFFSET):
+			if self.change_y < 0 : self.change_y = 0
+		if (self.center_y > screen_heigth - BORDERS_OFFSET):
+			if self.change_y > 0 : self.change_y = 0
+		super().update()
 	
 
