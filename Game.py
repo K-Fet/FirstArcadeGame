1c9b5e7c200f6821f2964e61968d9ae9f54640bf
bdbipbip <baptiste.dubarry21@gmail.com>
2018-07-19 23:32:14 +0200

    image player

--- Game.py
+++ Game.py
@@ -1,17 +1,21 @@
 import arcade 
 from data import *
-import player
+from player import *
 
 class Game(arcade.Window):
 
   def __init__(self,screen_width=SCREEN_WIDTH,screen_height=SCREEN_HEIGHT):
-    super().__init__(screen_width, screen_height)
+    super().__init__(screen_width, screen_height, fullscreen=True)
     arcade.set_background_color(arcade.color.AMAZON)
     self.player = None
+
+    width, heigth = self.get_size()
+    self.set_viewport(0, width, 0, heigth)
+
   
   
   def setup(self):
-    self.player = player()
+    self.player = player(0,0)
 
   def on_draw(self): 
     arcade.start_render()
@@ -20,17 +24,17 @@ class Game(arcade.Window):
   def update(self, delta_time):
     self.player.update()
 
-  def on_key_press(self, key):
+  def on_key_press(self, key, modifiers):
     if key == arcade.key.UP:
       self.player.change_y = MOVEMENT_SPEED
     if key == arcade.key.DOWN:
       self.player.change_y = -MOVEMENT_SPEED
     if key == arcade.key.LEFT:
       self.player.change_x = -MOVEMENT_SPEED
-    if key == arcade.key.UP:
+    if key == arcade.key.RIGHT:
       self.player.change_x = MOVEMENT_SPEED
 
-  def on_key_release(self, key):
+  def on_key_release(self, key, modifiers):
     if key == arcade.key.UP or key == arcade.key.DOWN:
       self.player.change_y = 0
     elif key == arcade.key.LEFT or key == arcade.key.RIGHT:
