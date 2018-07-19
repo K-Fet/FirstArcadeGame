108cca19c18c714d41a42bd049fb0a6f8206dba8
bdbipbip <baptiste.dubarry21@gmail.com>
2018-07-19 22:58:36 +0200

    player movement on key pressed

--- Game.py
+++ Game.py
@@ -1,21 +1,42 @@
 import arcade 
 from data import *
+import player
 
 class Game(arcade.Window):
 
   def __init__(self,screen_width=SCREEN_WIDTH,screen_height=SCREEN_HEIGHT):
     super().__init__(screen_width, screen_height)
     arcade.set_background_color(arcade.color.AMAZON)
+    self.player = None
   
   
   def setup(self):
+    self.player = player()
 
-    pass
+  def on_draw(self): 
+    arcade.start_render()
+    self.player.draw()
 
   def update(self, delta_time):
+    self.player.update()
 
-    pass
+  def on_key_press(self, key):
+    if key == arcade.key.UP:
+      self.player.change_y = MOVEMENT_SPEED
+    if key == arcade.key.DOWN:
+      self.player.change_y = -MOVEMENT_SPEED
+    if key == arcade.key.LEFT:
+      self.player.change_x = -MOVEMENT_SPEED
+    if key == arcade.key.UP:
+      self.player.change_x = MOVEMENT_SPEED
 
+  def on_key_release(self, key):
+    if key == arcade.key.UP or key == arcade.key.DOWN:
+      self.player.change_y = 0
+    elif key == arcade.key.LEFT or key == arcade.key.RIGHT:
+      self.player.change_x = 0
+
+    
 def main():
 
     game = Game()
