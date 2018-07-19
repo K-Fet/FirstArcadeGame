811a72dfb6e9983a49b4dd1c4ac44cd62c4ad1c2
bdbipbip <baptiste.dubarry21@gmail.com>
2018-07-19 21:58:17 +0200

    Black screen, first main methods init

--- Game.py
+++ Game.py
@@ -1,1 +1,26 @@
-import arcade from *
+import arcade 
+from data import *
+
+class Game(arcade.Window):
+
+  def __init__(self,screen_width=SCREEN_WIDTH,screen_height=SCREEN_HEIGHT):
+    super().__init__(screen_width, screen_height)
+    arcade.set_background_color(arcade.color.BLACK)
+  
+  
+  def setup(self):
+
+    pass
+
+  def update(self, delta_time):
+
+    pass
+
+def main():
+
+    game = Game()
+    game.setup()
+    arcade.run()
+
+if __name__ == "__main__":
+  main()
