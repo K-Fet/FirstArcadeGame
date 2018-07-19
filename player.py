2bafebfa0d5d3ff277d139cbbd989df8ac638411
Jean-Pierre MACHADO <mesjtarbes.direction@aplb.lan>
2018-07-19 10:27:11 +0200

    Separation

--- player.py
+++ player.py
@@ -1,7 +1,10 @@
 import arcade
 from MyGame import *
 import math
+from data import *
 
 class player(arcade.Sprite):
-	def __init__(self,scale):
-		super().__init__("Image/kfet.png",scale)
+	def __init__(self):
+		super().__init__("Image/kfet.png",SPRITE_SCALING_PLAYER)
+		self.center_x=50
+		self.center_y=50
