8b6159972b57959f792a6b11d2bb45d88f873f64
Jean-Pierre MACHADO <mesjtarbes.direction@aplb.lan>
2018-07-20 01:33:29 +0200

    Time

--- beer.py
+++ beer.py
@@ -1,8 +1,10 @@
 import arcade
 from data import *
+import random
 
 class beer(arcade.Sprite):
-	def __init__(self,center_x,center_y):
+	def __init__(self):
 		super().__init__("img/beer.png",SPRITE_SCALING_BEER)
-		self.center_x=center_x
-		self.center_y=center_y
+		self.center_x=random.randrange(SCREEN_WIDTH)
+		self.center_y=random.randrange(SCREEN_HEIGHT)
+		
