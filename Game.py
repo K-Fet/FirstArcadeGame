5b6e028846709d48b0fa216d44e16ada4a145fa2
bdbipbip <baptiste.dubarry21@gmail.com>
2018-07-20 11:26:14 +0200

    try stripline (unfinished)

--- Game.py
+++ Game.py
@@ -1,25 +1,30 @@
 import arcade 
 from data import *
 from player import *
+from stripline import *
 
 class Game(arcade.Window):
 
   def __init__(self,screen_width=SCREEN_WIDTH,screen_height=SCREEN_HEIGHT):
-    super().__init__(screen_width, screen_height, fullscreen=True)
+    super().__init__(screen_width, screen_height, fullscreen=FULLSCREEN)
     arcade.set_background_color(arcade.color.AMAZON)
     self.player = None
     self.screen_width,self.screen_height = self.get_size()
   
   
   def setup(self):
+    linepoints = ((0,0),(self.screen_width/2, self.screen_height/2))
+    self.stripline = stripline(linepoints)
     self.player = player(self.screen_width/2,self.screen_height/2)
 
   def on_draw(self): 
     arcade.start_render()
+    self.stripline.draw()
     self.player.draw()
 
   def update(self, delta_time):
     self.player.update(self.screen_width, self.screen_height)
+    
 
   def on_key_press(self, key, modifiers):
     if key == arcade.key.UP:
---
+++ stripline.py
@@ -0,0 +1,15 @@
+import arcade
+from data import *
+
+class stripline():
+	def __init__(self, points):
+		print("stripline init")
+		self.points = points
+		# self.center_x = (points[0][0] + points[1][0]) / 2
+		# self.center_y = (points[0][1] + points[1][1]) / 2  
+		self.line_points = self.get_points()
+		
+
+	def draw(self):
+		arcade.draw_line_strip(self.points, arcade.color.BLACK)
+
