618cfa314d2634cc13f39e69727fb4757bfd1d06
LucasMachad <lucas.pisanamachado@gmail.com>
2018-07-21 20:57:14 +0200

    ResponsiveMenu

--- Game.py
+++ Game.py
@@ -11,12 +11,12 @@ import operator
 
 
 def take_scores():
-    scores={}   
+  
     if os.path.exists(HIGHSCORE_FILE) and os.path.getsize(HIGHSCORE_FILE) > 0: # File exists
-        highscore = open(HIGHSCORE_FILE, "rb")
-        mon_depickler = pickle.Unpickler(highscore)
+        highscore_file = open(HIGHSCORE_FILE, "rb")
+        mon_depickler = pickle.Unpickler(highscore_file)
         scores = mon_depickler.load()
-        highscore.close()
+        highscore_file.close()
     else: # File does not exist
         scores = {}
     return scores
@@ -48,6 +48,48 @@ class Game(arcade.Window):
     self.menu=True
     self.highscore=False
 
+    self.line_break=screen_height/5
+
+    self.play_position_x=self.screen_width / 2
+    self.play_position_y=(3/4)*self.screen_height
+    self.play_height=self.screen_height/14
+    self.play_width=int(4*0.4*self.play_height) #Nb_lettre*largeur_lettre
+
+    self.menu_highscore_position_x=self.screen_width / 2
+    self.menu_highscore_position_y=(3/4)*self.screen_height - self.play_height - self.line_break
+    self.menu_highscore_height=self.screen_height/14
+    self.menu_highscore_width=int(10*0.4*self.menu_highscore_height)
+
+    self.quit_position_x=self.screen_width / 2
+    self.quit_position_y=(3/4)*self.screen_height - self.play_height - self.menu_highscore_height - 2*self.line_break
+    self.quit_height=self.screen_height/14
+    self.quit_width=int(4*0.4*self.quit_height)
+
+    self.game_over_position_x=self.screen_width / 2
+    self.game_over_height=self.screen_height/10
+    self.game_over_position_y=(self.screen_height / 2) + self.game_over_height
+
+    self.backmenu_position_x=(self.screen_width)*(1/10)
+    self.backmenu_position_y=(self.screen_height)*(1/10)
+    self.backmenu_height=self.screen_height/20
+    self.backmenu_width=int(10*0.4*self.backmenu_height)
+
+    self.backhighscore_position_x=(self.screen_width)*(9/10)
+    self.backhighscore_position_y=(self.screen_height)*(1/10)
+    self.backhighscore_height=self.screen_height/20
+    self.backhighscore_width=int(10*0.4*self.backhighscore_height)
+
+    self.restart_position_x=self.screen_width / 2
+    self.restart_position_y=(self.screen_height / 2) - self.game_over_height
+    self.restart_height=self.screen_height/20
+    self.restart_width=int(10*0.4*self.restart_height)
+
+    self.highscore_position_x=self.screen_width / 2
+    self.highscore_position_y=self.screen_height - self.line_break/4
+    self.highscore_height=54
+    self.highscore_width=int(10*0.4*self.highscore_height)
+
+
   
   def setup(self):
     linepoints = ((0,0),(self.screen_width/2, self.screen_height/2))
@@ -75,39 +117,49 @@ class Game(arcade.Window):
 
   def on_draw(self): 
     arcade.start_render()
+
     if self.game_over :
        
       score=self.score*self.total_time
 
       output = f"Game Over - Score : {int(self.score)}"
-      arcade.draw_text(output, GAME_OVER_POSITION_X, GAME_OVER_POSITION_Y, arcade.color.WHITE, GAME_OVER_HEIGHT, align="center",anchor_x="center",anchor_y="center")
+      arcade.draw_text(output, self.game_over_position_x, self.game_over_position_y, arcade.color.WHITE, self.game_over_height, align="center",anchor_x="center",anchor_y="center")
 
       output = "Click to restart"
-      arcade.draw_text(output, RESTART_POSITION_X, RESTART_POSITION_Y, arcade.color.WHITE, RESTART_HEIGHT, align="center",anchor_x="center",anchor_y="center")
-    
+      arcade.draw_text(output, self.restart_position_x, self.restart_position_y, arcade.color.WHITE, self.restart_height, align="center",anchor_x="center",anchor_y="center")
+
+      output = "menu"
+      arcade.draw_text(output,self.backmenu_position_x, self.backmenu_position_y, arcade.color.WHITE, self.backmenu_height,align="center",anchor_x="center",anchor_y="center")
+
+      output = "high score"
+      arcade.draw_text(output,self.backhighscore_position_x, self.backhighscore_position_y, arcade.color.WHITE, self.backhighscore_height,align="center",anchor_x="center",anchor_y="center")
+
     elif (self.menu):
       output = "Play"
-      arcade.draw_text(output, PLAY_POSITION_X, PLAY_POSITION_Y, arcade.color.WHITE, PLAY_HEIGHT,align="center",anchor_x="center",anchor_y="center")
+      arcade.draw_text(output, self.play_position_x, self.play_position_y, arcade.color.WHITE, self.play_height,align="center",anchor_x="center",anchor_y="center")
 
       output = "High Score"
-      arcade.draw_text(output, MENU_HIGHSCORE_POSITION_X, MENU_HIGHSCORE_POSITION_Y, arcade.color.WHITE, MENU_HIGHSCORE_HEIGHT,align="center",anchor_x="center",anchor_y="center")
+      arcade.draw_text(output, self.menu_highscore_position_x, self.menu_highscore_position_y, arcade.color.WHITE, self.menu_highscore_height,align="center",anchor_x="center",anchor_y="center")
 
       output = "Quit"
-      arcade.draw_text(output, QUIT_POSITION_X, QUIT_POSITION_Y, arcade.color.WHITE, QUIT_HEIGHT,align="center",anchor_x="center",anchor_y="center")
+      arcade.draw_text(output, self.quit_position_x, self.quit_position_y, arcade.color.WHITE, self.quit_height,align="center",anchor_x="center",anchor_y="center")
 
     elif (self.highscore):
       output = "High Score"
-      arcade.draw_text(output, HIGHSCORE_POSITION_X, HIGHSCORE_POSITION_Y,arcade.color.WHITE, HIGHSCORE_HEIGHT,align="center",anchor_x="center",anchor_y="center")
-      highscore=take_scores()
+      arcade.draw_text(output, self.highscore_position_x, self.highscore_position_y,arcade.color.WHITE, self.highscore_height,align="center",anchor_x="center",anchor_y="center")
+      list_highscore=take_scores()
       rank=1
       
-      sorted_highscore=sorted(highscore.items(),key=operator.itemgetter(1),reverse=True)
+      sorted_highscore=sorted(list_highscore.items(),key=operator.itemgetter(1),reverse=True)
 
       for pair in sorted_highscore :
         keys=re.sub('@','',pair[0])
         output=str(rank)+ " : " + keys + "  "+ str(pair[1])# + keys 
-        arcade.draw_text(output, HIGHSCORE_POSITION_X, HIGHSCORE_POSITION_Y - HIGHSCORE_HEIGHT*rank - LINE_BREAK*rank/6,arcade.color.WHITE,align="center",anchor_x="center",anchor_y="center")
+        arcade.draw_text(output, self.highscore_position_x, self.highscore_position_y - self.highscore_height*rank - self.line_break*(rank+2)/15,arcade.color.WHITE,align="center",anchor_x="center",anchor_y="center")
         rank+=1
+
+      output = "menu"
+      arcade.draw_text(output,self.backmenu_position_x, self.backmenu_position_y, arcade.color.WHITE, self.backmenu_height,align="center",anchor_x="center",anchor_y="center")
 
     else:
       self.stripline.draw()
@@ -151,7 +203,6 @@ class Game(arcade.Window):
         
         self.game_over=True   
         self.score=self.score*self.total_time
-        print(self.score)
 
         highscore=take_scores()
 
@@ -188,17 +239,28 @@ class Game(arcade.Window):
       
   def on_mouse_press(self,x,y,button,modifiers):
     if self.game_over :
-      self.setup()
-      self.game_over=False
+      if x < self.restart_position_x + self.restart_width and x > self.restart_position_x - self.restart_width and y < self.restart_position_y + self.restart_height and y > self.restart_position_y - self.restart_height :
+        self.setup()
+        self.game_over=False
+      elif x < self.backmenu_position_x + self.backmenu_width and x > self.backmenu_position_x - self.backmenu_width and y < self.backmenu_position_y + self.backmenu_height and y > self.backmenu_position_y - self.backmenu_height :
+         self.game_over=False
+         self.menu=True
+      elif x < self.backhighscore_position_x + self.backhighscore_width and x > self.backhighscore_position_x - self.backhighscore_width and y < self.backhighscore_position_y + self.backhighscore_height and y > self.backhighscore_position_y - self.backhighscore_height :
+         self.game_over=False
+         self.highscore=True
     elif self.menu:
-      if x < PLAY_POSITION_X + PLAY_WIDTH and x > PLAY_POSITION_X - PLAY_WIDTH and y < PLAY_POSITION_Y + PLAY_HEIGHT and y > PLAY_POSITION_Y - PLAY_HEIGHT :
+      if x < self.play_position_x + self.play_width and x > self.play_position_x - self.play_width and y < self.play_position_y + self.play_height and y > self.play_position_y - self.play_height :
         self.setup()
         self.menu=False
-      elif  x < MENU_HIGHSCORE_POSITION_X + MENU_HIGHSCORE_WIDTH and x > MENU_HIGHSCORE_POSITION_X - MENU_HIGHSCORE_WIDTH and y < MENU_HIGHSCORE_POSITION_Y + MENU_HIGHSCORE_HEIGHT and y > MENU_HIGHSCORE_POSITION_Y - MENU_HIGHSCORE_HEIGHT :
+      elif  x < self.menu_highscore_position_x + self.menu_highscore_width and x > self.menu_highscore_position_x - self.menu_highscore_width and y < self.menu_highscore_position_y + self.menu_highscore_height and y > self.menu_highscore_position_y - self.menu_highscore_height :
         self.menu=False
         self.highscore=True
-      elif  x < QUIT_POSITION_X + QUIT_WIDTH and x > QUIT_POSITION_X - QUIT_WIDTH and y < QUIT_POSITION_Y + QUIT_HEIGHT and y > QUIT_POSITION_Y - QUIT_HEIGHT :
+      elif  x < self.quit_position_x + self.quit_width and x > self.quit_position_x - self.quit_width and y < self.quit_position_y + self.quit_height and y > self.quit_position_y - self.quit_height :
         arcade.quick_run(0.5)
+    elif self.highscore :
+      if x < self.backmenu_position_x + self.backmenu_width and x > self.backmenu_position_x - self.backmenu_width and y < self.backmenu_position_y + self.backmenu_height and y > self.backmenu_position_y - self.backmenu_height :
+        self.highscore=False
+        self.menu=True
        
 
 def main():
